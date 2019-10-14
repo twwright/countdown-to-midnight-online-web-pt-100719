57d7d3803a293c92fed9b688142a5d657b8251a9
Thomas Wright <twwright@gmail.com>
2019-10-14 02:10:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
 
-def countdown
+
+def countdown(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  if count == 0
+    puts "HAPPY NEW YEAR!"
+  end
 end
